883b8b9e40c556f504951f4946117cbc4bf2e6ef
Dilan-Gunes <xDilangns@gmail.com>
2024-07-01 17:46:34 +0300

    Cicekotomasyonn
    Cicekotomasyon

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Cicekotomasyon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(711, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox2.Location = new System.Drawing.Point(711, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(193, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(602, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Türkçesi  :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(597, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "İngilizcesi :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(688, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Kayıt Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(688, 209);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Kayıt Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(688, 250);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 35);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Temizle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 167);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(653, 199);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(25, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(206, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Türkçesi  veya İngilizcesi :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox3.Location = new System.Drawing.Point(294, 113);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(193, 22);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(632, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Adet : ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox4.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox4.Location = new System.Drawing.Point(711, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(193, 22);
+            this.textBox4.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(937, 397);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Çiçek otomasyon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+namespace Cicekotomasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection cicek = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=cicekotomasyon.mdb.accdb");
+
+        //Button1 kayıt ekle  butonu
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            cicek.Open();
+            OleDbCommand komut = new OleDbCommand("INSERT INTO Cicekotomasyon(Turkce, Ingilizce, Adet) VALUES (@turkce, @ingilizce, @adet)", cicek);
+            komut.Parameters.AddWithValue("@turkce", textBox1.Text);
+            komut.Parameters.AddWithValue("@ingilizce", textBox2.Text);
+            komut.Parameters.AddWithValue("@adet", textBox4.Text);
+            komut.ExecuteNonQuery();
+            cicek.Close();
+            MessageBox.Show("Kayit eklendi", "Kayıt" );
+            tablo.Clear();
+            Listele();
+            for (int i = 0; i < Controls.Count; i++)
+            {
+                if (Controls[i] is TextBox)
+                {
+                    Controls[i].Text = "";
+                }
+            }
+        }
+
+        //dataGridView verilerin gözükeceyı yer, kare ...
+        DataTable tablo = new DataTable();
+
+        private void Listele() //listeleme yontemi
+        {
+            cicek.Open();
+            OleDbDataAdapter adtr= new OleDbDataAdapter("select * from cicekotomasyon", cicek);
+            adtr.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            cicek.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listele();
+
+        }
+
+        //Kayıt Silme butonu button2
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null && !dataGridView1.CurrentRow.IsNewRow)
+            {
+                DialogResult result = MessageBox.Show("Seçili kaydı silmek istediğinize emin misiniz?", "Kayıt Silme", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    string selectedTurkce = dataGridView1.CurrentRow.Cells["Turkce"].Value.ToString();
+
+                    cicek.Open();
+                    OleDbCommand komut = new OleDbCommand("DELETE FROM cicekotomasyon WHERE Turkce = @turkce", cicek);
+                    komut.Parameters.AddWithValue("@turkce", selectedTurkce);
+                    komut.ExecuteNonQuery();
+                    cicek.Close();
+
+                    MessageBox.Show("Kayıt Silindi");
+                    Listele();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silmek istediğiniz bir satırı seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        //TextBox3 arama satırı , arama yeri...
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = textBox3.Text.Trim();
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                cicek.Open();
+                OleDbDataAdapter adtr = new OleDbDataAdapter("SELECT * FROM cicekotomasyon WHERE Turkce LIKE @search OR Ingilizce LIKE @search", cicek);
+                adtr.SelectCommand.Parameters.AddWithValue("@search", "%" + searchText + "%");
+                DataTable tablo2 = new DataTable();
+                adtr.Fill(tablo2);
+                dataGridView1.DataSource = tablo2;
+                cicek.Close();
+            }
+            else
+            {
+                // Eğer arama kutusu boş ise, tüm verileri göstermek için Listele() metodunu çağırabilirsiniz.
+                Listele();
+            }
+        }
+
+        //button3 temizle butonu listbox dakı verileri temizler.
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    TextBox textBox = (TextBox)ctrl;
+                    textBox.Clear();
+                }
+            }
+        }
+    }
+}
